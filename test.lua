067b296bbb2bd1f5c0d811df5c8e8a0cfd4334e0
Craig Barnes <Cr@igBarn.es>
2014-09-20 19:47:52 +0100

    Move test data into separate directory

--- test.lua
+++ test.lua
@@ -1,15 +1,7 @@
 local ltcn = require "ltcn"
 local serpent = require "serpent"
 
-local input = [=[
-{
-    1, -2, 3, -0xff, -0, true, false, 'hi', "hello",
-    {'a','b','c'}, field1 = 55, ["field2"] = 44.0e-3, [false] = false,
-    ["\a\b\n\\'\""] = '"\n\t\r\\\r\n'
-}
-]=]
-
-local t, err = ltcn.parse(input, "")
+local t, err = ltcn.parse_file "test/t1.ltcn"
 
 if not t then
     io.stderr:write(err, "\n")
