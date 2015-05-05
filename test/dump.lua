b9220d47d7cf66d79227a312dba53747d7fd62f4
Craig Barnes <Cr@igBarn.es>
2015-05-05 10:58:58 +0100

    Clean up test/dump.lua

--- test/dump.lua
+++ test/dump.lua
@@ -2,11 +2,5 @@ local ltcn = require "ltcn"
 local serpent = require "serpent"
 local opts = {comment = false}
 
-local t, err = ltcn.parse_file(arg[1] or io.stdin)
-
-if t then
-    io.write(serpent.block(t, opts), "\n")
-else
-    io.stderr:write(err, "\n")
-    os.exit(1)
-end
+local t = assert(ltcn.parse_file(arg[1] or io.stdin))
+io.write(serpent.block(t, opts), "\n")
